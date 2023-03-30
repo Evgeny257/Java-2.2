a81d4ad175387a291d47f13b6c7da79874fdb395
Eugene <galanin.zhenia@yandex.ru>
2023-03-30 22:40:10 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,19 @@ package src;
 
 public class Main {
     public static void main(String[] args) {
+        int check = 100;
+        int add = 300;
+        int bonus = 0;
+        int sum;
+        if (add >= 1000) {
+            bonus = add / 100;
+            sum = check + add + bonus;
+            System.out.println("Your balance is " + sum + ". Your bonus is " + bonus);
+        }
+        else {
+            sum = check + add;
+            System.out.println("Your balance is " + sum + ". Your bonus is " + bonus);
+        }
 
     }
 }
